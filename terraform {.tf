5aef04b61a7bf8e6cecd34b587a5dbda178fa307
Iamneo <techadmin@iamneo.ai>
2023-09-22 08:08:10 +0000

    Backup Commit

--- terraform {.tf
+++ terraform {.tf
@@ -1,5 +1,20 @@
 terraform {
     required_providers{
-        
+        aws={
+            source="hashicorp/aws"
+            version="~>4.0"
+        }
     }
+}
+provider "aws"{
+    region="ap-southeast-1"
+    access_key="AKIAQAR763YWE7XXIYNT"
+    secret_key="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"
+}
+resource "aws_instance" "aws"{
+    ami="ami-072f48a9ed4f1bbda"
+    instance_type="t2.micro"
+}
+output "public_ip"{
+    value = aws_instance.aws.public_ip
 }
